32f675c92a878deff6bc3fdf94d1196ff458b78f
Mohammad Talim <talimmohammad116@gmail.com>
2024-07-13 16:03:51 +0530

    feat: implement pokemon search

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import "./App.css";
 import PokemonCard from "./components/PokemonCard";
 import { getPokemonData, getPokemonDetails } from "./services/pokemonService";
@@ -6,6 +6,7 @@ import { Pokemon } from "./types/Pokemon";
 
 const App = () => {
   const [pokemonList, setPokemonList] = useState<Pokemon[]>([]);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
@@ -18,11 +19,26 @@ const App = () => {
     fetchData();
   }, []);
 
+  const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const filteredPokemon = pokemonList.filter((pokemon) =>
+    pokemon.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <div className="App">
       <h1>Pokémon List</h1>
+      <input
+        type="text"
+        placeholder="Search Pokémons by name"
+        value={search}
+        onChange={handleSearch}
+        className="search-bar"
+      />
       <div className="pokemon-container">
-        {pokemonList.map((pokemon) => (
+        {filteredPokemon.map((pokemon) => (
           <PokemonCard key={pokemon.id} pokemon={pokemon} />
         ))}
       </div>
